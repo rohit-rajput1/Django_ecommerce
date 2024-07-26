b077307c2250f4990f2eb098d81721214b78f2b2
rohit31rajput2001 <rohit31rajput2001@gmail.com>
2024-07-26 19:13:58 +0530

    urls added

--- payment/urls.py
+++ payment/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('payment_success', views.payment_success, name='payment_success'),
-    path('checkout',views.checkout,name='checkout')
+    path('checkout',views.checkout,name='checkout'),
+    path('billing_info',views.billing_info,name='billing_info'),
+    path('process_order',views.process_order,name='process_order'),
 ]
